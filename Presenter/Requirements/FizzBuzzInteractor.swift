a2c69fb23c72b5a09d72ab87fa90dba7a8298d72
Pierre Rougeot <pierre.rougeot@fabernovel.com>
2021-07-02 10:30:35 +0200

    Create Presenter framework
    Add FizzBuzzInteractor protocol

---
+++ Presenter/Requirements/FizzBuzzInteractor.swift
@@ -0,0 +1,16 @@
+//
+//  FizzBuzzInteractor.swift
+//  Presenter
+//
+//  Created by Pierre Rougeot on 02/07/2021.
+//
+
+import Entity
+
+public protocol FizzBuzzInteractor {
+    /// processes any fizz buzz request and record the operation for statistics
+    /// - Parameter request: request
+    /// - returns result
+    func process(request: FizzBuzzRequest) -> FizzBuzzResult
+    var statistics: FizzBuzzStatistics? { get }
+}
